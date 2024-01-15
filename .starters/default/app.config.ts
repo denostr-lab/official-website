882c9e0c53f1211820b59171c8199161368296e0
1519574824@qq.com <1519574824@qq.com>
2024-01-15 19:14:40 +0800

    fix: 修正twitter 链接和 app 链接

--- .starters/default/app.config.ts
+++ .starters/default/app.config.ts
@@ -4,7 +4,7 @@ export default defineAppConfig({
     description: 'Denostr: your cloud-native nostr relay',
     image: 'https://github.com/denostr-lab/official-website/assets/8678079/d1d329c8-3c33-4141-ba1f-1b6fef3fa24c',
     socials: {
-      twitter: 'RealDenostr', // denostr-lab
+      twitter: 'denostr_com', // denostr-lab
       github: 'denostr-lab/denostr'
     },
     github: {
